90a43edf36d94217312e5c1d9870f3b8a5c9b8d9
Max Farr <maxandersfarr@gmail.com>
2024-03-09 14:52:30 -0800

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,34 @@ import { createContext, useContext, useEffect, useRef, useState } from "react";
 import * as Tone from "tone";
 import * as d3 from "d3";
 
-const BATCHES = 80;
-const GRAPH_WIDTH = 700;
-const GRAPH_HEIGHT = 400;
+const GRAPH_WIDTH_PX = 700;
+const GRAPH_HEIGHT_PX = 400;
+
 const GRAPH_UPDATE_MS = 40;
 const PITCH_DETECT_MS = 100;
-const WINDOW_SIZE = BATCHES * 128;
 
 const MAX_TAU = 600;
+const BATCHES = 80;
+const WINDOW_SIZE = BATCHES * 128;
 
 const INPUT_SAMPLE_RATE = 44100.0;
 
 const SamplesContext = createContext();
+
+const NOTE_LETTERS = [
+  "A",
+  "A#",
+  "B",
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+];
 
 const lineGreenHex = "#73AD21";
 const backgroundHex = "#202020";
@@ -133,8 +149,6 @@ function ToggleGraphButton({ setEnabled }) {
 }
 
 function Graph({ samples }) {
-  const [, setSucks] = useState();
-
   let line = d3
     .line()
     .x((d) => {
@@ -145,7 +159,6 @@ function Graph({ samples }) {
     });
 
   setInterval(() => {
-    //setSucks();
     let datavals = [];
     if (!(samples.current === undefined || samples.current.length === 0)) {
       samples.current.map((value, index) => {
@@ -161,12 +174,12 @@ function Graph({ samples }) {
       .append("path")
       .datum(datavals)
       .attr("d", line)
-      .attr("width", GRAPH_WIDTH)
-      .attr("height", GRAPH_HEIGHT)
+      .attr("width", GRAPH_WIDTH_PX)
+      .attr("height", GRAPH_HEIGHT_PX)
       .attr(
         "transform",
-        `translate(0, ${GRAPH_HEIGHT / 2}) scale(${
-          GRAPH_WIDTH / (128 * BATCHES)
+        `translate(0, ${GRAPH_HEIGHT_PX / 2}) scale(${
+          GRAPH_WIDTH_PX / (128 * BATCHES)
         }, 1.0)`
       )
       .style("stroke", lineGreenHex)
@@ -176,7 +189,11 @@ function Graph({ samples }) {
 
   return (
     <div className="frame">
-      <svg className="graph" width={GRAPH_WIDTH} height={GRAPH_HEIGHT}></svg>
+      <svg
+        className="graph"
+        width={GRAPH_WIDTH_PX}
+        height={GRAPH_HEIGHT_PX}
+      ></svg>
     </div>
   );
 }
@@ -188,17 +205,6 @@ function ACF(tau, samples) {
   }
   for (let i = 0; i < WINDOW_SIZE - tau; i++) {
     accum += samples.current[i] * samples.current[i + tau];
-  }
-  return accum;
-}
-
-function SDF(tau, samples) {
-  let accum = 0.0;
-  if (WINDOW_SIZE > samples.current.length) {
-    return 0.0;
-  }
-  for (let i = 0; i < WINDOW_SIZE - tau; i++) {
-    accum += Math.pow(samples.current[i] - samples.current[i + tau], 2);
   }
   return accum;
 }
@@ -216,20 +222,6 @@ function NSDF(tau, samples) {
 }
 
 const MATH_DENOM = Math.log10(Math.pow(2, 1 / 12));
-const NOTE_LETTERS = [
-  "A",
-  "A#",
-  "B",
-  "C",
-  "C#",
-  "D",
-  "D#",
-  "E",
-  "F",
-  "F#",
-  "G",
-  "G#",
-];
 
 function PitchDetector({ samples }) {
   const svgRef = useRef();
@@ -255,28 +247,23 @@ function PitchDetector({ samples }) {
   const k = 0.9;
 
   useEffect(() => {
-    // create svg element
+    // draw axis
     var svg = d3.select(".detectorFrame").select(".graph");
 
-    // Create the scale
-    var x = d3
-      .scaleLinear()
-      .domain([1, MAX_TAU]) // This is what is written on the Axis: from 0 to 100
-      .range([0, GRAPH_WIDTH]); // This is where the axis is placed: from 100px to 800px
+    var x = d3.scaleLinear().domain([1, MAX_TAU]).range([0, GRAPH_WIDTH_PX]);
 
-    // Draw the axis
     svg
       .append("g")
-      .attr("transform", `translate(0,${GRAPH_HEIGHT * 0.9})`) // This controls the vertical position of the Axis
+      .attr("transform", `translate(0,${GRAPH_HEIGHT_PX * 0.9})`)
       .call(d3.axisBottom(x));
 
     svg
       .append("line")
       .style("stroke", lineGreenHex)
       .attr("x1", 0)
-      .attr("y1", GRAPH_HEIGHT / 2)
-      .attr("x2", GRAPH_WIDTH)
-      .attr("y2", GRAPH_HEIGHT / 2)
+      .attr("y1", GRAPH_HEIGHT_PX / 2)
+      .attr("x2", GRAPH_WIDTH_PX)
+      .attr("y2", GRAPH_HEIGHT_PX / 2)
       .style("stroke-dasharray", "3, 3");
   });
 
@@ -287,20 +274,12 @@ function PitchDetector({ samples }) {
       let positiveZeroCrossings = [];
       let negativeZeroCrossings = [];
 
-      //console.log(maxval);
       if (samples.current === undefined || samples.current.length === 0) {
         return;
       }
 
-      //console.log("like, hello!!!!");
       for (let i = 0; i < MAX_TAU; i++) {
         let val = NSDF(i + 1, samples);
-        //console.log("val " + i.toString() + ": ", val);
-        //console.log("before");
-        //console.log("val: ", val, "maxval: ", maxval);
-
-        //console.log("after");
-        //console.log("val: ", val, "maxval: ", maxval);
         datavals.push([i, val]);
       }
 
@@ -314,6 +293,7 @@ function PitchDetector({ samples }) {
         }
       }
 
+      // TODO: figure this mess out
       let calculatePeak = true;
       if (positiveZeroCrossings.length === 0) {
         //throw new Error("initDetection: no positive zero crossings");
@@ -353,8 +333,6 @@ function PitchDetector({ samples }) {
         }
       }
 
-      //console.log("maxval: ", maxval);
-
       d3.select(".detectorFrame").select(".graph").selectAll("path").remove();
       let svg = d3
         .select(".detectorFrame")
@@ -362,34 +340,17 @@ function PitchDetector({ samples }) {
         .append("path")
         .datum(datavals)
         .attr("d", line)
-        .attr("width", GRAPH_WIDTH)
-        .attr("height", GRAPH_HEIGHT)
+        .attr("width", GRAPH_WIDTH_PX)
+        .attr("height", GRAPH_HEIGHT_PX)
         .attr(
           "transform",
-          `translate(0, ${GRAPH_HEIGHT / 2}) scale(${GRAPH_WIDTH / 600}, 1.0)`
+          `translate(0, ${GRAPH_HEIGHT_PX / 2}) scale(${
+            GRAPH_WIDTH_PX / 600
+          }, 1.0)`
         )
         .style("stroke", lineGreenHex)
         .style("stroke-width", 3)
         .style("fill", "none");
-
-      // for (let i = 25; i < 600; i++) {
-      //   // console.log(
-      //   //   "i: ",
-      //   //   i,
-      //   //   "maxval * k: ",
-      //   //   maxval * k,
-      //   //   "datavals[i]: ",
-      //   //   datavals[i]
-      //   // );
-      //   if (
-      //     datavals[i][1] < datavals[i - 1][1] &&
-      //     datavals[i - 1][1] > maxval * k
-      //   ) {
-      //     setPeakTau(i + 1);
-      //     console.log("set peak ", i + 1);
-      //     return;
-      //   }
-      // }
     }, PITCH_DETECT_MS);
   }
 
@@ -397,8 +358,8 @@ function PitchDetector({ samples }) {
     <div className="detectorFrame" style={widgetStyle}>
       <svg
         className="graph"
-        width={GRAPH_WIDTH}
-        height={GRAPH_HEIGHT}
+        width={GRAPH_WIDTH_PX}
+        height={GRAPH_HEIGHT_PX}
         ref={svgRef}
       ></svg>
       <p>
