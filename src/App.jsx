5a0aad86d1fb9c4194e4f90f2af70e3ec7eeedc7
Max Farr <maxandersfarr@gmail.com>
2024-03-17 18:19:10 -0700

    more cleanup

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>try going to /dev you sillyhead</div>;
+}
+
+export default App;
--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,8 +1,6 @@
-import { createContext, useEffect, useRef, useState } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import * as Tone from "tone";
-import * as d3 from "d3";
 import DevTools from "./DevTools";
+import App from "./App";
 
 const router = createBrowserRouter([
   {
@@ -17,10 +15,6 @@ const router = createBrowserRouter([
 
 function AppRouter() {
   return <RouterProvider router={router} />;
-}
-
-function App() {
-  return <div>try going to /dev you sillyhead</div>;
 }
 
 export default AppRouter;
